35640a2fd186b47d8e7cd97e026e57b8556a77d8
Zach Auten <zachauten@gmail.com>
2025-12-20 04:57:14 +0000

    add about page

---
+++ routes/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div class="about-container">
+      <p>
+        Hi, I'm Zach! I am a full stack software engineer out of North Carolina
+        with a great domain name. 😄
+      </p>
+      <img class="about-img" src="/img/about/zach.avif" />
+    </div>
+  );
+}
